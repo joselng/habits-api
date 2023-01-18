1b956a8cfb8041853d0950fd8665a704ae6d378d
José Leonardo Nascimento Garcia <leogarcia.net@hotmail.com>
2023-01-18 14:37:00 -0300

    added fastify/cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+
 import { PrismaClient } from "@prisma/client"
 
 const app = fastify();
 const prisma = new PrismaClient();
+
+app.register(cors);
 
 app.get("/", async () => {
     const habits = await prisma.habit.findMany()
